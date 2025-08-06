ef209631d0a201e8c042c275b9b9f99a40fb0bf9
zzj <2421411703@qq.com>
2025-08-06 11:06:17 +0800

    代码

--- BasicGrammar/List/lab2.py
+++ BasicGrammar/List/lab2.py
@@ -2,5 +2,23 @@ my_list=[1,2,3,4,5,"student"]
 
 index=my_list.index(2)
 
+append_list=["sa","hcw"]
+
+my_list.insert(2,"zzj")
+
 print(my_list[-1])
 print(index)
+print(my_list)
+
+my_list.extend(append_list)
+print(my_list)
+
+print("------------------------------------")
+del my_list[2]
+print(my_list)
+
+my_list.insert(2,"zzj")
+print(my_list)
+
+my_list.pop(2)
+print(my_list)
